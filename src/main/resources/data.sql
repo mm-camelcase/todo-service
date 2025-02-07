9b17b905036998b9cc462b5c624c053b6e63bf2d
Mark Mitchell <markmitchell451@gmail.com>
2025-02-07 16:46:03 +0000

    impl

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO todo (id, task, completed) VALUES (1, 'Finish homework', false);
+INSERT INTO todo (id, task, completed) VALUES (1, 'Water the plants', false);
 INSERT INTO todo (id, task, completed) VALUES (2, 'Clean the house', true);
-INSERT INTO todo (id, task, completed) VALUES (3, 'Buy groceries', false);
-INSERT INTO todo (id, task, completed) VALUES (4, 'Call the doctor', true);
+INSERT INTO todo (id, task, completed) VALUES (3, 'Go for a run', false);
+INSERT INTO todo (id, task, completed) VALUES (4, 'Call mother', true);
 INSERT INTO todo (id, task, completed) VALUES (5, 'Pay bills', false);
